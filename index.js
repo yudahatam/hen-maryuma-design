396dbc03f9abbe986607d53e11e9e6c1d0e2d815
Yuda Hatam <yehudahatam@gmail.com>
2021-05-03 20:09:23 +0300

    aaasdasd

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var PORT = process.env.PORT || 8080
+app.use('/static', express.static('public'));
+app.get('/', function (req, res) {
+    return res.send("Hello Yuda");
+})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
