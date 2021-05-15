030f6db9999d6f395f1acf07e30381da7903c0e8
Yuda Hatam <yehudahatam@gmail.com>
2021-05-15 19:40:06 +0300

    adding files

---
+++ schema.sql
@@ -0,0 +1,9 @@
+create table requests(
+    Id serial,
+    name varchar(255),
+    email varchar(255),
+    phone varchar(255),
+    message varchar(255),
+
+    primary key (id)
+);
---
+++ seeds.sql
@@ -0,0 +1,5 @@
+INSERT into requests(name email phone message)
+VALUES("yuda","yuda@yuda.yuda","0528790549","yuda yuda ya yuda"),
+    ("checn","yuda@yuda.yuda","9875564","yuda hjklhjl ya eterwy"),
+    ("edne","yuda@asd.yuda","0528790549","yuda asd ya yuda"),
+    ("ira","asd@yuda.yuda","045454528790549","gjlg yuda hjlhjl yuda");
